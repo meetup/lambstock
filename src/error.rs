acc22535479b1fdb1a9ea5130cea4b0fda6e93f5
softprops <d.tangren@gmail.com>
2018-08-27 10:39:23 -0400

    fmt

--- src/error.rs
+++ src/error.rs
@@ -4,20 +4,26 @@ use rusoto_resourcegroupstaggingapi::GetResourcesError;
 /// Failure types
 #[derive(Fail, Debug)]
 pub enum Error {
-  #[fail(display = "{}", _0)]
-  Listing(#[cause] ListFunctionsError),
-  #[fail(display = "{}", _0)]
-  Tags(#[cause] GetResourcesError),
+    #[fail(display = "{}", _0)]
+    Listing(
+        #[cause]
+        ListFunctionsError
+    ),
+    #[fail(display = "{}", _0)]
+    Tags(
+        #[cause]
+        GetResourcesError
+    ),
 }
 
 impl From<ListFunctionsError> for Error {
-  fn from(err: ListFunctionsError) -> Self {
-    Error::Listing(err)
-  }
+    fn from(err: ListFunctionsError) -> Self {
+        Error::Listing(err)
+    }
 }
 
 impl From<GetResourcesError> for Error {
-  fn from(err: GetResourcesError) -> Self {
-    Error::Tags(err)
-  }
+    fn from(err: GetResourcesError) -> Self {
+        Error::Tags(err)
+    }
 }
